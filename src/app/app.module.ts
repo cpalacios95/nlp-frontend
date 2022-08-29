60b7b91d981138e6959b432f20f49a662a137681
cristian <example@gmail.com>
2022-08-29 15:52:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/pages/login/login.component';
+import { SingupComponent } from './components/pages/singup/singup.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AdminComponent } from './components/home/admin/admin.component';
+import { ProfessorComponent } from './components/home/professor/professor.component';
+import { StudentComponent } from './components/home/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingupComponent,
+    NavbarComponent,
+    AdminComponent,
+    ProfessorComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
